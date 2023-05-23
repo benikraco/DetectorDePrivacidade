33777a9f9b9a44d10f330182edd5d42219c3b72b
benikraco <benikraco@gmail.com>
2023-05-22 22:01:07 -0300

    Cookies check

--- background/background.js
+++ background/background.js
@@ -1,53 +1,41 @@
-let tabs = {};
-let thirdPartyUrls = new Set();  // Create a new Set to store the third-party URLs
-let cookiesCount = 0;
+let thirdPartyUrls = new Set();
+let cookieDetails = [];
 
-// Listener for any change in tab URL
-browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
-    tabs[id] = tab;
-});
-
+// Listener for third-party requests
 browser.webRequest.onBeforeRequest.addListener(
-    (details) => {
-        console.log('Received web request:', details);
+  (details) => {
+    const initiator = details.initiator || details.originUrl;
+    const target = new URL(details.url);
 
-        // Check if the tab exists and its URL is set
-        if (tabs[details.tabId] && tabs[details.tabId].url) {
-            let requestHost = new URL(details.url).host;
-            let tabHost = new URL(tabs[details.tabId].url).host;
-
-            if (requestHost !== tabHost) {
-                console.log('Third-party request detected:', details.url);
-                thirdPartyUrls.add(details.url);  // Add the detected third-party URL to the Set
-            }
-        }
-    },
-    { urls: ['<all_urls>'] },
-    ['blocking']
-);
-
-browser.webRequest.onHeadersReceived.addListener(
-    (details) => {
-        // Inspect the headers in the HTTP response
-        for (let header of details.responseHeaders) {
-            if (header.name.toLowerCase() === 'set-cookie') {
-                // Count each 'Set-Cookie' header
-                cookiesCount++;
-            }
-        }
-    },
-    { urls: ['<all_urls>'] },
-    ['responseHeaders']
-);
-
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.method === 'getThirdPartyUrls') {
-        sendResponse({data: Array.from(thirdPartyUrls)});
-    } else if (request.method === 'getCookiesCount') {
-        sendResponse({data: cookiesCount});
-    } else {
-        sendResponse({data: null});
+    if (initiator && (new URL(initiator).origin !== target.origin)) {
+      thirdPartyUrls.add(target.origin);
     }
+  },
+  { urls: ['<all_urls>'] }
+);
+
+// Listener for Cookie changes
+browser.cookies.onChanged.addListener((changeInfo) => {
+  const cookie = changeInfo.cookie;
+
+  // Check if the cookie was removed
+  if (changeInfo.removed) {
+    cookieDetails = cookieDetails.filter(item => item.name !== cookie.name && item.domain !== cookie.domain);
+  } else {
+    cookieDetails.push({
+      name: cookie.name,
+      value: cookie.value,
+      domain: cookie.domain,
+      session: cookie.session
+    });
+  }
 });
 
-
+// Listener for extension messages
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.method === 'getThirdPartyUrls') {
+    sendResponse({data: Array.from(thirdPartyUrls)});
+  } else if (request.method === 'getCookies') {
+    sendResponse({data: cookieDetails});
+  }
+});
--- popup/popup.js
+++ popup/popup.js
@@ -1,27 +1,46 @@
-document.addEventListener("DOMContentLoaded", function() {
-  browser.runtime.sendMessage({method: "getThirdPartyUrls"})
-  .then((response) => {
-      renderContent(response.data);
-  });
-
-  // Get cookies count
-  browser.runtime.sendMessage({method: "getCookiesCount"})
-  .then((response) => {
-      document.getElementById('cookies-count').textContent = 'Cookies: ' + response.data;
-  });
-});
-
+// Function to render the list of third-party URLs
 const renderContent = (data) => {
   const content = document.getElementById('third-party-urls');
+  content.innerHTML = '';
 
   data.forEach((url) => {
-      const listItem = document.createElement('li');
-      listItem.textContent = url;
-      content.appendChild(listItem);
+    const listItem = document.createElement('li');
+    listItem.textContent = url;
+    content.appendChild(listItem);
   });
 };
 
+// Function to render the list of Cookies
+const renderCookies = (data) => {
+  const content = document.getElementById('cookie-list');
+  content.innerHTML = '';
+
+  data.forEach((cookie) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = `${cookie.name} (Domain: ${cookie.domain}, Session: ${cookie.session ? 'Yes' : 'No'})`;
+    content.appendChild(listItem);
+  });
+
+  // Update the cookie count
+  document.getElementById('cookie-count').innerText = `Total de cookies: ${data.length}`;
+};
+
+// Event listener for the Clear Log button
 document.getElementById('clear-logs').addEventListener('click', () => {
-  browser.runtime.sendMessage({method: 'clearThirdPartyUrls'});
   document.getElementById('third-party-urls').innerHTML = '';
+  document.getElementById('cookie-list').innerHTML = '';
+  document.getElementById('cookie-count').innerText = '0 Cookies';
+});
+
+// On page load, get the list of third-party URLs and Cookies
+document.addEventListener('DOMContentLoaded', function() {
+  browser.runtime.sendMessage({method: 'getThirdPartyUrls'})
+    .then((response) => {
+      renderContent(response.data);
+    });
+
+  browser.runtime.sendMessage({method: 'getCookies'})
+    .then((response) => {
+      renderCookies(response.data);
+    });
 });
